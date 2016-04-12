866c98c186e555d9ee3237b9bb6b21348dfb82a7
nice2nice <805940799@qq.com>
2016-04-12 16:51:32 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "805940799@qq.com"
+git config --global user.name "nice2nice"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://nice2nice.github.io
